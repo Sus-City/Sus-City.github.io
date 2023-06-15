144c2fcb0fd7ce041293e5b68bde9a2eb6baf481
Ryan Tung <88029861+Ryan-Tung@users.noreply.github.com>
2023-06-15 21:56:58 +0800

    badwords file

---
+++ GAME-PAGE/badwords.js
@@ -0,0 +1,4 @@
+//censoring system
+//IF YOU ARE A USER LOOKING AT THIS PLEASE KNOW THAT THESE ARE NAUGHTY BAD WORDS
+const profanities = ["fuck", "cunt", "cock", "cocksucker", "dickhead", "ballsack", "balls", "fuckwit", "damn", "dick", "penis", "shit", "bitch", "fucking", "motherfucking", "mofo", "tf", "bastard", "tits", ]; // Naughty words
+export default profanities;
